2abd5e296fa527ada0b18664cf77969e76577f8f
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-12 23:34:56 +0530

    fix: await User.findOne to ensure user retrieval is handled correctly

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -8,7 +8,7 @@ connect()
 export async function POST(request: NextRequest) {
     // extract data from token
     const userId = await getDataFromToken(request)
-    const user = User.findOne({_id:userId}).select("-password")
+    const user = await User.findOne({_id:userId}).select("-password")
     //check if there is no user
     return NextResponse.json({
         message: "User found successfully",
