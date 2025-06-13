e73de5184ac68a7cd2278c2e703da0aa0770261d
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-13 16:50:54 +0530

    feat: enhance login and verify email pages with improved error handling and user feedback

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-function page() {
+'use client'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { toast } from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+
+export default function LoginPage() {
+  const router = useRouter()
+  const [user, setUser] = useState({
+    email:"",
+    password:"",
+  })
+
+  const [buttonDisabled, setButtonDisabled] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const onLogin = async () => {
+    try {
+      setLoading(true)
+      const response = await axios.post("/api/users/login", user)
+      console.log("Login success", response.data)
+      router.push("/profile")
+
+    } catch (error:any) {
+      console.log("Signup failed");
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false)
+    } else {
+      setButtonDisabled(true)
+    }
+  }, [user])
+
   return (
-    <div>page</div>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1 className='text-4xl mb-2 p-3 bg-gradient-to-r from-blue-500 via-blue-300 to-blue-100 bg-clip-text text-transparent'>{loading ? "Processing" : "Login"}</h1>
+      <hr />
+
+      <label htmlFor="email">Email</label>
+      <input
+      className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black bg-white'
+      id= 'email'
+      value={user.email}
+      onChange={(e) => setUser({...user, email: e.target.value})}
+      placeholder='Enter your email'
+      type="text" />
+      <hr />
+
+      <label htmlFor="password">Password</label>
+      <input
+      className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black bg-white'
+      id= 'password'
+      value={user.password}
+      onChange={(e) => setUser({...user, password: e.target.value})}
+      placeholder='Enter a password'
+      type="password" />
+
+      <button
+        onClick={onLogin}
+        className='text-xs p-1 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-white bg-gray-600 hover:bg-gray-700'
+        >
+        {buttonDisabled ? "Please enter all credentials" : "Login"}
+      </button>
+      <Link 
+      className='text-2xl p-3 bg-gradient-to-r from-blue-500 via-blue-300 to-blue-100 bg-clip-text text-transparent border-2 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+      href={"/signup"}>Signup</Link>
+    </div>
   )
 }
-
-export default page
--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -17,6 +17,7 @@ export default function VerifyEmailPage() {
         try {
             await axios.post('/api/users/verifyemail', {token})
             setverified(true)
+            setError(false)
         } catch (error:any) {
             setError(true)
             console.log(error.response.data);
@@ -25,6 +26,7 @@ export default function VerifyEmailPage() {
     }
 
     useEffect(() => {
+        setError(false)
         const urlToken = window.location.search.split("=")[1]
         setToken(urlToken || "")
         
@@ -34,6 +36,7 @@ export default function VerifyEmailPage() {
     }, [])
 
     useEffect(() => {
+        setError(false)
         if (token.length > 0){
             verifyUserEmail()
         }
