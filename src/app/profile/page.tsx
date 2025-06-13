56ced6ee22bbb65eb1f2b64a6055f7f5f66a8e3e
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-13 16:56:12 +0530

    feat: create ProfilePage component with styled header

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProfilePage() {
+  return (
+    <div className='text-3xl items-center justify-center bg-gradient-to-r from-blue-500 via-blue-300 to-blue-100 bg-clip-text text-transparent m-81'><h1 className='text-center'>Profile Page</h1></div>
+  )
+}
+
+export default ProfilePage
