1fcc9c51fb7453db7b008a30a7c91469e60f1098
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-13 13:45:30 +0530

    next auth project

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from 'react'
 
-function page() {
+export default function SignupPage() {
+  const {user, setUser} = useState({
+    email:"",
+    password:"",
+    username:""
+  })
+
+  const [buttonDisabled, setButtonDisabled] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const onSignup = async () => {
+    try {
+      
+    } catch (error:any) {
+      console.log("Signup failed");
+      
+    }
+  }
   return (
     <div>page</div>
   )
 }
-
-export default page
